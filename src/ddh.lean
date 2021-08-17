07bdc3f2855f957951c4974e1faaef51f5db923a
Joey Lupo <jlupohockey@gmail.com>
2021-08-16 19:51:19 -0500

    Change A to D in ddh

--- src/ddh.lean
+++ src/ddh.lean
@@ -16,7 +16,7 @@ variables (G : Type) [fintype G] [group G]
           (g : G) (g_gen_G : ∀ (x : G), x ∈ subgroup.gpowers g) 
           (q : ℕ) [fact (0 < q)] (G_card_q : fintype.card G = q) 
           -- check Mario, 0 < q necessary for fintype.card?
-          (A : G → G → G → pmf (zmod 2))
+          (D : G → G → G → pmf (zmod 2))
 
 include g_gen_G G_card_q
 
@@ -24,7 +24,7 @@ def DDH0 : pmf (zmod 2) :=
 do 
   x ← uniform_zmod q,
   y ← uniform_zmod q,
-  b ← A (g^x.val) (g^y.val) (g^(x.val * y.val)),
+  b ← D (g^x.val) (g^y.val) (g^(x.val * y.val)),
   pure b
 
 def DDH1 : pmf (zmod 2) := 
@@ -32,12 +32,12 @@ do
   x ← uniform_zmod q,
   y ← uniform_zmod q,
   z ← uniform_zmod q,
-  b ← A (g^x.val) (g^y.val) (g^z.val),
+  b ← D (g^x.val) (g^y.val) (g^z.val),
   pure b
 
-local notation `Pr[DDH0]` := (DDH0 G g g_gen_G q G_card_q A 1 : ℝ)
-local notation `Pr[DDH1]` := (DDH1 G g g_gen_G q G_card_q A 1 : ℝ)
+local notation `Pr[DDH0(D)]` := (DDH0 G g g_gen_G q G_card_q D 1 : ℝ)
+local notation `Pr[DDH1(D)]` := (DDH1 G g g_gen_G q G_card_q D 1 : ℝ)
 
-def DDH (ε : nnreal) : Prop := abs (Pr[DDH0] - Pr[DDH1]) ≤ ε
+def DDH (ε : nnreal) : Prop := abs (Pr[DDH0(D)] - Pr[DDH1(D)]) ≤ ε
 
 end DDH
