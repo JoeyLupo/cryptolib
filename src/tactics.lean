2e385f90e24296c5859601a30f048dfa30dd61d1
Joey Lupo <jlupohockey@gmail.com>
2021-08-18 00:33:11 -0500

    Change to greek letter types in tactic

--- src/tactics.lean
+++ src/tactics.lean
@@ -1,9 +1,9 @@
 import measure_theory.probability_mass_function
 
-variables {A B : Type}
+variables {α β : Type}
 
-lemma bind_skip' (p : pmf A) (f g : A → pmf B) : 
-  (∀ (a : A), f a = g a) → p.bind f = p.bind g :=
+lemma bind_skip' (p : pmf α) (f g : α → pmf β) : 
+  (∀ (a : α), f a = g a) → p.bind f = p.bind g :=
 begin
   intro ha, 
   ext,
@@ -11,8 +11,8 @@ begin
   simp_rw ha,
 end
 
-lemma bind_skip_const' (pa : pmf A) (pb : pmf B) (f : A → pmf B) : 
-  (∀ (a : A), f a = pb) → pa.bind f = pb :=
+lemma bind_skip_const' (pa : pmf α) (pb : pmf β) (f : α → pmf β) : 
+  (∀ (a : α), f a = pb) → pa.bind f = pb :=
 begin
   intro ha, 
   ext,
